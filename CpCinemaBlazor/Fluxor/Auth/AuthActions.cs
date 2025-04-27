54eff4bc61f6ba2675b967f41403676242d30972
timOon849 <supersprait@gmail.com>
2025-04-27 08:54:35 +0300

    new

--- CpCinemaBlazor/Fluxor/Auth/AuthActions.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace CpCinemaBlazor.Fluxor.Auth
-{
-    public record LoginSuccessAction(string JwtToken, string UserRole, DateTime TokenExpiration);
-    public record LogoutAction;
-    public record UpdateTokenAction(string NewToken, DateTime NewExpiration);
-}
--- CpCinemaBlazor/Program.cs
+++ CpCinemaBlazor/Program.cs
@@ -48,6 +48,7 @@ if (!app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 app.UseRouting();
+app.UseAuthorization();
 app.UseStaticFiles();
 app.UseAntiforgery();
 
